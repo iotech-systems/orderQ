34c6d5146d1692a849778dfbcffb9cb0b1cc8cbd
erik <erik@iotech.systems>
2023-04-24 03:25:25 +0200

    add orderQ.wsgi

--- run_orderq.py
+++ run_orderq.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-import json
-import os
+
+import json, os
 import flask as _f, setproctitle
 import configparser as _cp
 
@@ -16,7 +16,6 @@ INI_FILE = f"{ROOT_DIR}/conf/site.ini"
 if not os.path.exists(INI_FILE):
    raise FileExistsError(INI_FILE)
 
-
 INI: _cp.ConfigParser = _cp.ConfigParser()
 if len(INI.read(INI_FILE)) != 1:
    raise Exception("IniLoadingError")
@@ -27,19 +26,20 @@ APP_NAME: str = INI.get("FLASK", "APP_NAME")
 app = _f.Flask(APP_NAME, static_url_path=""
    , static_folder=STATIC_DIR, template_folder=TMPLS_DIR)
 
+
 @app.route("/info", methods=["GET"])
 def info():
    remote_ip = _f.request.remote_addr
    return _f.render_template("info.html", remote_ip=remote_ip)
 
-@app.route("/get/called_numbers/<shopid>")
+@app.route("/get/called_numbers/<shopid>", methods=["GET"])
 def read_shop_numbers(shopid):
    from core.shopOps import shopOps
    ops: shopOps = shopOps(INI, shopid)
    called_orders: [] = ops.get_called_orders()
    return json.dumps(called_orders)
 
-@app.route("/set/called_numbers/<shopid>/<numbers>")
+@app.route("/set/called_numbers/<shopid>/<numbers>", methods=["POST"])
 def set_called_numbers(shopid, numbers):
    from core.shopOps import shopOps
    ops: shopOps = shopOps(INI, shopid)

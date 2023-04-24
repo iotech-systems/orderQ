7f2b1dd4f46d98b8dbc6eba11654c67c73b35acd
erik <erik@iotech.systems>
2023-04-24 03:09:49 +0200

    add orderQ.wsgi

---
+++ core/shopOps.py
@@ -0,0 +1,35 @@
+
+import redis
+import configparser as _cp
+
+
+class shopOps(object):
+
+   def __init__(self, ini: _cp.ConfigParser, shopid: int):
+      self.ini: _cp.ConfigParser = ini
+      self.shopid = shopid
+      self.red: redis.Redis = None
+
+   def __del__(self):
+      try:
+         if self.red is not None:
+            self.red.close()
+      except:
+         pass
+
+   def get_called_orders(self) -> []:
+      self.__set_conn__()
+      rval = self.red.hget(f"ORDER_CALL_TABLES", self.shopid)
+      return rval
+
+   def update_called_orders(self, shopid, numbers):
+      self.__set_conn__()
+      rval = self.red.hset(f"ORDER_CALL_TABLES", key=shopid, value=numbers)
+      return rval
+
+   def __set_conn__(self):
+      host = self.ini.get("REDIS", "HOST")
+      port: int = self.ini.getint("REDIS", "PORT")
+      pwd: str = self.ini.get("REDIS", "PWD")
+      db: int = self.ini.getint("REDIS", "CALLED_NUMBERS_DB_IDX")
+      self.red: redis.Redis = redis.Redis(host=host, port=port, db=db, password=pwd)
--- run_orderq.py
+++ run_orderq.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python3
-
+import json
+import os
 import flask as _f, setproctitle
 import configparser as _cp
 
 
+# -- -- -- --
 ROOT_DIR = "/opt/iotech/orderQ"
+if os.uname()[1] == "3cpo":
+   ROOT_DIR = "/home/erik/__github__/orderq"
+# -- -- -- --
 TMPLS_DIR = f"{ROOT_DIR}/tmpls"
 STATIC_DIR = f"{ROOT_DIR}/static"
+INI_FILE = f"{ROOT_DIR}/conf/site.ini"
+if not os.path.exists(INI_FILE):
+   raise FileExistsError(INI_FILE)
 
-FLASK_PORT: int = 8022
 
-APP_NAME = "orderQ"
+INI: _cp.ConfigParser = _cp.ConfigParser()
+if len(INI.read(INI_FILE)) != 1:
+   raise Exception("IniLoadingError")
+
+
+FLASK_PORT: int = INI.getint("FLASK", "PORT")
+APP_NAME: str = INI.get("FLASK", "APP_NAME")
 app = _f.Flask(APP_NAME, static_url_path=""
    , static_folder=STATIC_DIR, template_folder=TMPLS_DIR)
 
@@ -19,10 +32,24 @@ def info():
    remote_ip = _f.request.remote_addr
    return _f.render_template("info.html", remote_ip=remote_ip)
 
+@app.route("/get/called_numbers/<shopid>")
+def read_shop_numbers(shopid):
+   from core.shopOps import shopOps
+   ops: shopOps = shopOps(INI, shopid)
+   called_orders: [] = ops.get_called_orders()
+   return json.dumps(called_orders)
 
-# == == == == == == == == == == == == == == == == == == == == == == == == == ==
+@app.route("/set/called_numbers/<shopid>/<numbers>")
+def set_called_numbers(shopid, numbers):
+   from core.shopOps import shopOps
+   ops: shopOps = shopOps(INI, shopid)
+   ops.update_called_orders(shopid, numbers)
+   return "OK"
+
+
+# == == == == == == == == == == == == == == == == == == == == == == ==
 # -- -- [ start app here ] -- --
 if __name__ == "__main__":
    setproctitle.setproctitle(APP_NAME)
    app.run(host="0.0.0.0", port=FLASK_PORT, debug=False)
-# == == == == == == == == == == == == == == == == == == == == == == == == == ==
+# == == == == == == == == == == == == == == == == == == == == == == ==

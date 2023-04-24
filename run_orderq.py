c8eebedf3edb268a3ce40d116f000e7b76687e46
erik <erik@iotech.systems>
2023-04-24 03:44:51 +0200

    add orderQ.wsgi

--- run_orderq.py
+++ run_orderq.py
@@ -36,8 +36,7 @@ def info():
 def read_shop_numbers(shopid):
    from core.shopOps import shopOps
    ops: shopOps = shopOps(INI, shopid)
-   called_orders: [] = ops.get_called_orders()
-
+   called_orders = ops.get_called_orders()
    d: {} = {"err": 0, "called": called_orders}
    return json.dumps(d), 200, {"content-type": "application/json"}
 
